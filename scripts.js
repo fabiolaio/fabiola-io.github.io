76e313cc69e043640e844879ef2d9347bcbe8a70
Fabiola Larios <53882456+fabiolaio@users.noreply.github.com>
2023-06-20 22:21:01 -0400

    update code again >.< js

--- scripts.js
+++ scripts.js
@@ -3,10 +3,15 @@ $(document).ready(function() {
     var $artworkFrame = $('#artwork-frame');
     var $windowTitle = $('#window-title');
 
+
+
+    // Ensure that the window is hidden at the start
     $artworkWindow.hide();
 
     $('.window').draggable();
 
+
+    // Update the clock immediately, then every 1 second
     updateClock();
     setInterval(updateClock, 1000);
 
@@ -22,44 +27,44 @@ $(document).ready(function() {
     $('.title-bar button').click(function() {
         $artworkWindow.hide();
     });
-
-    var $startButton = $('#start-button');
-    var $startMenu = $('#start-menu');
-    var $startMenuItems = $('.start-menu-item');
-
-$startButton.click(function(event) {
-    $startMenu.toggle();
-    event.stopPropagation();
 });
 
-$startMenuItems.click(function(event) {
-    console.log("Start menu item clicked");  // Add this line
-    var url = $(this).data('url');
+var $startButton = $('#start-button');
+var $startMenu = $('#start-menu');
+var $startMenuItems = $('.start-menu-item');
+
+$startButton.click(function() {
+    $startMenu.toggle(); // show or hide the start menu
+});
+
+$startMenuItems.click(function() {
+    var artworkUrl = $(this).data('url');
     var title = $(this).data('title');
 
-    if (url.startsWith('http')) {
-        window.open(url, '_blank');
-    } else {
-        $artworkFrame.attr('src', url);
-        $windowTitle.text(title);
-        $artworkWindow.show();
-    }
+    $artworkFrame.attr('src', artworkUrl);
+    $windowTitle.text(title);
+    $artworkWindow.show();
+    $startMenu.hide(); // hide the start menu
+});
 
-    $startMenu.hide();
-    event.stopPropagation();
+$(document).click(function(event) {
+    // If the click was not on the start button or start menu (or any of their children), hide the start menu
+    if (!$(event.target).closest('#start-button, #start-menu').length) {
+        $startMenu.hide();
+    }
 });
 
 function updateClock() {
     let date = new Date();
+
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let ampm = hours >= 12 ? 'PM' : 'AM';
 
     hours = hours % 12;
-    hours = hours ? hours : 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
     minutes = minutes < 10 ? '0' + minutes : minutes;
 
     let strTime = hours + ':' + minutes + ' ' + ampm;
 
     document.getElementById('clock').textContent = strTime;
-}
