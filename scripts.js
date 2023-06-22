6b68e06ebd0cc66b973170af16cc8c537cba5e9c
Fabiola Larios <53882456+fabiolaio@users.noreply.github.com>
2023-06-22 17:07:03 -0400

    update menu and submenu

--- scripts.js
+++ scripts.js
@@ -3,13 +3,10 @@ $(document).ready(function() {
     var $artworkFrame = $('#artwork-frame');
     var $windowTitle = $('#window-title');
 
-
-
     // Ensure that the window is hidden at the start
     $artworkWindow.hide();
 
     $('.window').draggable();
-
 
     // Update the clock immediately, then every 1 second
     updateClock();
@@ -18,7 +15,7 @@ $(document).ready(function() {
     $('.icon').click(function() {
         var artworkUrl = $(this).data('url');
         var artworkName = $(this).find('p').text();
-    
+
         $artworkFrame.attr('src', artworkUrl);
         $windowTitle.text(artworkName);
         $artworkWindow.show();
@@ -27,31 +24,37 @@ $(document).ready(function() {
     $('.title-bar button').click(function() {
         $artworkWindow.hide();
     });
-});
 
-var $startButton = $('#start-button');
-var $startMenu = $('#start-menu');
-var $startMenuItems = $('.start-menu-item');
+    var $startButton = $('#start-button');
+    var $startMenu = $('#start-menu');
+    var $startMenuItems = $('.start-menu-item');
 
-$startButton.click(function() {
-    $startMenu.toggle(); // show or hide the start menu
-});
+    $startButton.click(function() {
+        $startMenu.toggle(); // show or hide the start menu
+    });
 
-$startMenuItems.click(function() {
-    var artworkUrl = $(this).data('url');
-    var title = $(this).data('title');
+    $startMenuItems.click(function(e) {
+        e.stopPropagation();
 
-    $artworkFrame.attr('src', artworkUrl);
-    $windowTitle.text(title);
-    $artworkWindow.show();
-    $startMenu.hide(); // hide the start menu
-});
+        if($(this).children('ul').length > 0) {
+            $(this).children('ul').toggle(); // show or hide the nested menu
+        } else {
+            var artworkUrl = $(this).data('url');
+            var title = $(this).data('title');
 
-$(document).click(function(event) {
-    // If the click was not on the start button or start menu (or any of their children), hide the start menu
-    if (!$(event.target).closest('#start-button, #start-menu').length) {
-        $startMenu.hide();
-    }
+            $artworkFrame.attr('src', artworkUrl);
+            $windowTitle.text(title);
+            $artworkWindow.show();
+            $startMenu.hide(); // hide the start menu
+        }
+    });
+
+    $(document).click(function(event) {
+        // If the click was not on the start button or start menu (or any of their children), hide the start menu
+        if (!$(event.target).closest('#start-button, #start-menu').length) {
+            $startMenu.hide();
+        }
+    });
 });
 
 function updateClock() {
@@ -61,11 +64,4 @@ function updateClock() {
     let minutes = date.getMinutes();
     let ampm = hours >= 12 ? 'PM' : 'AM';
 
-    hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-
-    let strTime = hours + ':' + minutes + ' ' + ampm;
-
-    document.getElementById('clock').textContent = strTime;
-}
+    hours =
