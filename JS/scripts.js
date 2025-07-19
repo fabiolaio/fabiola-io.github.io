a7323e523b7b6438704e758fe354e9c5750ffbf5
Fabiola Larios <53882456+fabiolaio@users.noreply.github.com>
2025-07-19 13:37:04 -0400

    update fix error news

--- JS/scripts.js
+++ JS/scripts.js
@@ -84,3 +84,20 @@ function updateClock() {
 }
 
 
+function updateClock() {
+    let date = new Date();
+
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let ampm = hours >= 12 ? 'PM' : 'AM';
+
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+
+    let strTime = hours + ':' + minutes + ' ' + ampm;
+
+    document.getElementById('clock').textContent = strTime;
+}
+
+
